1112db5e956f646bb33cf43fe50d9dd742990ca0
bredfern-xteam <69480496+bredfern-xteam@users.noreply.github.com>
2020-09-16 14:11:15 -0700

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+
+const myserver = express()
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new WebSocketServer({ server: myserver });
+
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    // Broadcast any received message to all clients
+    console.log('received: %s', message);
+    wss.broadcast(message);
+  });
+
+  ws.on('error', () => ws.terminate());
+});
+
+wss.broadcast = function (data) {
+  this.clients.forEach(function (client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+
+
+  
